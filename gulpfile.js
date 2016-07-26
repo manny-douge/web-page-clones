3b307b2e974705ee0e34912f6e17564d8430015d
Emmanuel Douge <aidendouge@Emmanuels-MacBook-Air-2.local>
2016-07-26 00:17:42 -0400

    fixed browser sync reloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-gulp.task('browserSync',function(){
-    browserSync.init({
-        server: {baseDir:'app'},
-                    })
-            });
+gulp.task('browserSync',function()
+{
+    browserSync.init
+    ({
+        server: "app"
+    })
+});
 gulp.task('watch',['browserSync'], function()
 {
+    //console.log('Reloaded');
     gulp.watch('app/*.html',browserSync.reload);
     gulp.watch('app/js/**/*.js',browserSync.reload);
+    gulp.watch('app/css/*.css',browserSync.reload);
 });
 gulp.task('hello',function()
 {
